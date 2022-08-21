26c2c4dc14951cdc170f360458211b836bc3bcfe
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-21 19:07:52 +0300

    Message converter configuration

---
+++ src/main/java/guru/springframework/msscjms/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package guru.springframework.msscjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    /*
+    When we send a message to JMS, Spring will convert it to a JMS text message, and it will take the object and convert it to a JSON payload.
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
