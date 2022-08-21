f91cc9456177597cd06c12a41e41547cae38ab70
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-21 18:50:12 +0300

    Created simple POJO for JMS messaging

---
+++ src/main/java/guru/springframework/msscjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 42l;
+
+    private UUID id;
+    private String message;
+}
