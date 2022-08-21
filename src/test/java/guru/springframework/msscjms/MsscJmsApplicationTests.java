7681af0a2f640e5188a2c53e79cc1d1a3ccd31cf
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-21 18:36:58 +0300

    Project creation

---
+++ src/test/java/guru/springframework/msscjms/MsscJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
